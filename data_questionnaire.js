78d618cccaa791d07a46404680784858011f777a
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2020-05-31 13:54:14 +0200

    Initialisation du repo avec premiers travaux
    - Bases du CSS de la page
    - Header et footer
    - Début du javascript du questionnaire

---
+++ data_questionnaire.js
@@ -0,0 +1,5 @@
+data_questionnaire_json = '[' +
+    '{"question" : "Quel type de jeu préférez-vous ?", "reponses" : [["Corps à corps",-1], ["Mixte",0], ["Distance",1]]},' +
+    '{"question" : "Quel style de jeu préférez-vous ?", "reponses" : [["Taper fort",-2], ["Soigner",-1], ["Résister",0], ["Arriver au bon moment",1]]},' +
+    '{"question" : "Comment préférez-vous jouer ?", "reponses" : [["En solo",-1], ["À deux",1]]}' +
+    ']';
